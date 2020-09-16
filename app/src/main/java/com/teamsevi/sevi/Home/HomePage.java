f436964ee04cbd0b50ea4f9495ae5ffbaac3a5ad
crv342 <chiragrvv@gmail.com>
2020-09-16 16:44:56 +0530

    all files added

--- app/src/main/java/com/teamsevi/sevi/Home/HomePage.java
+++ app/src/main/java/com/teamsevi/sevi/Home/HomePage.java
@@ -1,16 +1,21 @@
  package com.teamsevi.sevi.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
 import com.teamsevi.sevi.R;
 
 public class HomePage extends AppCompatActivity {
 
+    DrawerLayout drawerlayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
     }
+
 }
