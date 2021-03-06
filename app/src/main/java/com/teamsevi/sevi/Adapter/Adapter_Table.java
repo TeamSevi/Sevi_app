c135129019d5098c8a9bfd1ffb49a1d46f542a3b
kuldeep-mkm <kuldeepmungalpara123@gmail.com>
2021-03-06 16:07:38 +0530

    menu

---
+++ app/src/main/java/com/teamsevi/sevi/Adapter/Adapter_Table.java
@@ -0,0 +1,44 @@
+package com.teamsevi.sevi.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.teamsevi.sevi.Model.Model_Table;
+import com.teamsevi.sevi.R;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter_Table extends FirebaseRecyclerAdapter<Model_Table,Adapter_Table.holder> {
+
+
+    public Adapter_Table(@NonNull FirebaseRecyclerOptions<Model_Table> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull holder holder, int position, @NonNull Model_Table model) {
+    holder.textView.setText(model.getNumer());
+    }
+
+    @NonNull
+    @Override
+    public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.table,parent,false);
+        return new holder(view);
+    }
+
+    class holder extends RecyclerView.ViewHolder{
+        TextView textView;
+
+        public holder(@NonNull View itemView) {
+            super(itemView);
+            textView =(TextView)itemView.findViewById(R.id.t4);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/teamsevi/sevi/Table.java
@@ -0,0 +1,48 @@
+package com.teamsevi.sevi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+import com.teamsevi.sevi.Adapter.Adapter_Table;
+import com.teamsevi.sevi.Adapter.Adapter_menu;
+import com.teamsevi.sevi.Model.Model_Table;
+import com.teamsevi.sevi.Model.Model_menu;
+
+public class Table extends AppCompatActivity {
+RecyclerView recyclerView;
+Adapter_Table adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table);
+        SharedPreferences shared = getSharedPreferences("HotelSession", MODE_PRIVATE);
+        String hotelid = shared.getString("hotelid", "");
+        recyclerView = (RecyclerView)findViewById(R.id.rev2);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        FirebaseRecyclerOptions<Model_Table> options =
+                new FirebaseRecyclerOptions.Builder<Model_Table>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Hotel").child(hotelid).child("tables"), Model_Table.class)
+                        .build();
+        adapter = new Adapter_Table(options);
+        recyclerView.setAdapter(adapter);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
